3838f2bd1f529ea922436eb3e52f5462b162b7ed
Anton Fefilov <atomdev@icloud.com>
2025-06-01 23:33:36 +0400

    Update setup script to streamline environment initialization and training session
    This commit modifies the `setup.sh` script by commenting out the repository cloning command and adding commands to create a new tmux session for training. It now includes commands to activate the virtual environment and run the memory-efficient training script, enhancing the setup process for users.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # git clone https://github.com/antonfefilov/assignment1-basics.git
-cd assignment1-basics
+# cd assignment1-basics
 pip install uv
 uv sync
-source .venv/bin/activate
 mkdir -p data
 cd data
 wget https://github.com/antonfefilov/assignment1-basics/releases/download/v1.0.0/TinyStoriesV2-GPT4-train.txt
@@ -14,4 +13,10 @@ gunzip owt_train.txt.gz
 wget https://huggingface.co/datasets/stanford-cs336/owt-sample/resolve/main/owt_valid.txt.gz
 gunzip owt_valid.txt.gz
 cd ..
+tmux new-session -d -s "train"
+tmux send-keys "source .venv/bin/activate" C-m
+tmux send-keys "python train_memory_efficient.py --input data/owt_train.txt --processes 16 --vocab-size 32000" C-m
+tmux attach-session -t "train"
+
+# source .venv/bin/activate
 # ./train.sh -i data/TinyStoriesV2-GPT4-train.txt -p 4 -v 10000
